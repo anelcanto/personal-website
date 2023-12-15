00a8bb96d8902ddfa15ba63390b566921e1314f0
Anel Canto <ac@anelcanto.com>
2023-12-14 20:47:17 -0700

    Add published_at

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -59,13 +59,13 @@ class BlogPostsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_blog_post
-    @blog_post = BlogPost.find(params[:id])
+    @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path, notice: 'Blog post not found'
   end
 
   # Only allow a list of trusted parameters through.
   def blog_post_params
-    params.require(:blog_post).permit(:title, :body)
+    params.require(:blog_post).permit(:title, :body, :published_at)
   end
 end
--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,2 +1,5 @@
 class BlogPost < ApplicationRecord
+  scope :draft, -> { where(published_at: nil) }
+  scope :published, -> { where.call('published_at: <= ?', Time.current) }
+  scope :scheduled, -> { where.call('published_at: > ?', Time.current) }
 end
